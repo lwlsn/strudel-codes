e49f2897e25b3640869f3493987a6151af97c065
Lizzie <e-wilson1@hotmail.com>
2024-06-19 16:56:36 +0100

    Create workshop.js

---
+++ workshop.js
@@ -0,0 +1,160 @@
+/*  PART 1 - SOME COMPOSITION FUNDAMENTALS */
+
+/* 1.1 - Review from previous workshop.. ⏰ 5 mins */ 
+
+//update the tempo
+let setbpm = t => setcps(t/4/60)
+
+setbpm(151)
+
+// make a simple pattern
+// sound("bd AlesisSR16_sd*4") // mini-notation basics, find a sample, repeat events 
+
+// more complex patterns
+//sound("bd:1 hh ~ cp") // select samples using :, add rests using ~, 
+
+
+// Euclidean rhythms 
+
+// sound("casio:1(3,8)")
+
+
+/*
+E(2,5)=[x . x . .] is a thirteenth century Persian rhythm called Khafif-e-ramal [34]. It is also the metric
+pattern of the second movement of Tchaikovsky’s Symphony No. 6 [17]. When it is started on the second
+onset ([x . . x .]) it is the metric pattern of Dave Brubeck’s Take Five as well as Mars from The Planets by
+Gustav Holst [17].
+E(3,4)=[x . x x] is the archetypal pattern of the Cumbia from Colombia [20], as well as a Calypso rhythm
+from Trinidad [13]. It is also a thirteenth century Persian rhythm called Khalif-e-saghil [34], as well as the
+trochoid choreic rhythmic pattern of ancient Greece [21].
+E(3,5)=[x . x . x], when started on the second onset, is another thirteenth century Persian rhythm by the
+name of Khafif-e-ramal [34], as well as a Rumanian folk-dance rhythm [25].
+E(3,7)=[x . x . x . .] is a Ruchenitza rhythm used in a Bulgarian folk-dance [24]. It is also the metric
+pattern of Pink Floyd’s Money [17].
+E(3,8)=[x . . x . . x .] is the Cuban tresillo pattern discussed in the preceding [15].
+E(4,7)=[x . x . x . x] is another Ruchenitza Bulgarian folk-dance rhythm [24].
+E(4,9) = [x . x . x . x . .] is the Aksak rhythm of Turkey [6]. It is also the metric pattern used by Dave
+Brubeck in his piece Rondo a la Turk [17].
+E(4,11) = [x . . x . . x . . x .] is the metric pattern used by Frank Zappa in his piece titled Outside Now [17].
+E(5,6)=[x . x x x x] yields the York-Samai pattern, a popular Arab rhythm, when started on the second
+onset [30].
+E(5,7)=[x . x x . x x] is the Nawakhat pattern, another popular Arab rhythm [30].
+E(5,8)=[x . x x . x x .] is the Cuban cinquillo pattern discussed in the preceding [15]. When it is started
+on the second onset it is also the Spanish Tango [13] and a thirteenth century Persian rhythm, the Al-saghilal-sani [34].
+E(5,9)=[x . x . x . x . x] is a popular Arab rhythm called Agsag-Samai [30]. When started on the second
+onset, it is a drum pattern used by the Venda in South Africa [26], as well as a Rumanian folk-dance
+rhythm [25].
+E(5,11)=[x . x . x . x . x . .] is the metric pattern used by Moussorgsky in Pictures at an Exhibition [17].
+E(5,12) = [x . . x . x . . x . x .] is the Venda clapping pattern of a South African children’s song [24].
+E(5,16) = [x . . x . . x . . x . . x . . . .] is the Bossa-Nova rhythm necklace of Brazil. The actual BossaNova rhythm usually starts on the third onset as follows: [x . . x . . x . . . x . . x . .] [31]. However, there are
+other starting places as well, as for example [x . . x . . x . . x . . . x . .] [3].
+E(7,8) = [x . x x x x x x] is a typical rhythm played on the Bendir (frame drum), and used in the accompaniment of songs of the Tuareg people of Libya [30].
+
+
+Check out the paper here: 
+https://archive.bridgesmathart.org/2005/bridges2005-47.pdf
+*/
+
+// Adding functions
+// sound("bd:1 cp:2").fast(2)
+// sound("bd:1 cp:2").slow(2)
+// sound("bd:1 cp:2").chop(8)
+// sound("bd sd hh cp").rev()
+// sound("bd:1 bd:2").every(4, fast(4))
+
+//  Layers of sound
+
+// sound("bd sd ~ cp:1, casio:0(3,8) casio:1")
+
+// Layers of sound with stack 
+
+// stack(
+//   sound("bd:1 sd ~ cp").color("[#5BC0EB #FDE74C #9BC53D ]"), 
+//   sound("bd:4(3,8) AlesisSR16_sd*4").color("[#C3423F #7E2E84]")
+// )
+// .punchcard()
+
+/* 1.2 - Playing sounds from the digital selves sample pack ⏰ 5 mins */ 
+
+// How do i download it? 
+
+//
+
+// How do i load it into strudel? 
+samples({
+  k:  ['snd/k/DNC_Kick.wav' ], 
+  hjdsynth: ['snd/hjdsynth/bass-armor.wav','snd/hjdsynth/bell-dingo.wav','hjdsynth/bell-anomal.wav'],
+  gel: ['snd/glass_gel_1.wav', 'snd/glass_gel_2.wav, snd/glass_gel_3.wav', 'snd/glass_gel_4.wav', 'snd/glass_gel_5.wav']
+}, 'github:lwlsn/digital-selves-samples/main/');
+
+sound("k")
+
+// samples({
+//     k: [
+//       'samples/alk/0001.wav', 
+//       'samples/alk/0001b.wav',
+//       'samples/alk/0001c.wav',
+//       'samples/k/DNC_Kick.wav'
+//     ],
+//     cp: [
+//       'samples/lcp/cp2.mp3'
+//     ]
+//   }, 'github:lwlsn/lwlsn.github.io/master/');
+
+
+// stack(sound("{k:3*2 k:3 ~ k:1 ~ k:3 ~  k:3}%4").color("[#5BC0EB*4 #FDE74C ]")
+//     //sound("~ ~ ~ cp").color("[#C3423F]")
+//       )
+// .punchcard()
+
+
+
+
+/* 1.3 - Example drum beat  ⏰ 10 mins */ 
+s("{k ~ rim ~, ~ ~ hh}%<4 8 16 8>")
+  .chunk(4, fast(2))
+  .jux(rev)
+  .off(.25, mul(speed(.8)))
+  .superimpose(
+    x=>x.vowel("<a <e i>>/4").room(2).slow(2).late(.25).color("#00ff0050")
+  )
+  .lastOf(8, mul(speed(-2)))
+  .color("[#0000ff50 #ffff0020]*3")
+  .punchcard()
+
+/* 1.4 -  Playing with harmony (strudel-based synthesisers) ⏰ 10 mins  */
+  
+// stack(
+//   n("[0 1 3 [~ 4] 5 6 [~ 7] 8]")
+//     .sometimesBy("<0.1 0.5>".slow(16), rev())
+//     .chord("Am C".slow(4))
+//     .voicing()
+//     .s(choose("sawtooth", "square"))
+//     .gain("0.6")
+//     .add(note("<[0,-12,12] [0,-12.2, <5 7>]>".slow(8)))
+//     .add(note("0, -12.1"))
+//     .lpf(saw.rangex(100,"<200 1000 5000 20000>".slow(2)).slow(8))
+//     .lpe(sine.rangex(10,1))
+//     .room(saw.rangex(0.1,1).slow(8))
+//     .delay("0.1:0.6:0.7"),
+//     sound("{k:3*2 k:3 ~ k:1 ~ k:3 ~  k:3}%4"),
+//     sound("~ ~ ~ cp")
+//   )
+
+
+
+
+
+/* 1.5 - Playing with midi (samples)?  */
+
+
+/*  PART 2 - GOING DEEPER */
+
+// 2.1 -- Repetition (and it's exploitation)
+
+// 2.2 -- Interference 
+
+// 2.3 -- Call and response 
+
+// 2.4 -- Putting this together into a composition
+
